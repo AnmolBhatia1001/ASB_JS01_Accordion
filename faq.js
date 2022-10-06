d07db8a3cc0482d6179342d7672dad35c09d5491
AnmolSinghBhatia1001 <anmolsinghbhatia1001@gmail.com>
2022-10-06 18:50:53 +0530

    Funcionality Added

--- faq.js
+++ faq.js
@@ -18,7 +18,8 @@ const faqData = [
   },
 ];
 
-const accordianBody;
+const accordianBody = document.querySelector(".accordian_body");
+
 const faqs = [];
 
 function showFaq() {
@@ -26,11 +27,86 @@ function showFaq() {
 }
 
 function createFaq() {
+
+  faqData.forEach(e => {
+
+    // create FAQ div
+    const faqDiv = document.createElement("div");
+    faqDiv.setAttribute("class", "faq")
+
+    // creating FAQ_header
+    const faqHeaderDiv = document.createElement("div");
+    faqHeaderDiv.setAttribute("class", "faq_header")
+
+    // creating FAQ content (p)
+    const faqContent = document.createElement("p")
+    faqContent.setAttribute("class", "hidden")
+
+    // appending faq div inside accordianBody
+    accordianBody.append(faqDiv)
+
+    // appending header & content inside faq div
+    faqDiv.append(faqHeaderDiv)
+    faqDiv.append(faqContent)
+
+    // creating element for faq_header
+    const headingH3 = document.createElement("h3")
+    const plusImg = document.createElement("img")
+
+    // appending  header & img inside faq_header
+    faqHeaderDiv.append(headingH3);
+    faqHeaderDiv.append(plusImg);
+
+    // injecting text & attributes inside elements
+    const h3Text = document.createTextNode(e.question)
+    headingH3.append(h3Text)
+
+    plusImg.setAttribute("width", "28px")
+    plusImg.setAttribute("src", "https://img.icons8.com/ios-glyphs/344/plus--v1.png")
+    
+    const contentText = document.createTextNode(e.answer);
+    faqContent.append(contentText)
+
+
+  })
+
   
 }
+createFaq()
+
+
+
+const header = document.querySelectorAll(".faq_header");
+const para = document.querySelectorAll("p");
+console.log(header, para);
+
+
+for (let i = 0; i < header.length; i++) {
+  header[i].addEventListener("click", () => {
+      for (let j = 0; j < para.length; j++) {
+          if (i == j) {
+              para[j].classList.toggle("hidden")
+          } 
+      }
+  });
+}
+
+
+
+// header.forEach(e => {
+//   e.addEventListener("click", ()=>{
+
+//     para.forEach(f => {
+//       f.classList.toggle("hidden")
+//     })
+    
+//   })
+// })
 
 function btnStatusUpdate() {
   
 }
+console.log(accordianBody);
+
 
 
